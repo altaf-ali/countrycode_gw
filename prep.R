4f9f96f2bc5e286431b368addf11d0600693157c
Altaf Ali <altaf@fastmail.com>
2016-07-13 19:55:02 +0100

    support for GW codes

--- prep.R
+++ prep.R
@@ -3,12 +3,14 @@ library(readr)
 library(dplyr)
 document()
 
+rm(list = ls())
 countrycode_data = read.csv('data/countrycode_data.csv', stringsAsFactors=FALSE, na.strings='')
+
 countrycode_data <- countrycode_data %>%
   rename(country.name = country_name) %>%
   mutate(`regex` = iconv(`regex`, to='UTF-8'))
 
-gwcodes <- read_tsv("data/iisystem.dat", col_names = c("gwn", "gwc", "gw_country", "gw_start", "gw_end"),
+gwcodes <- read_tsv("data/gwcodes.tsv", col_names = c("gwn", "gwc", "gw_country", "gw_start", "gw_end"),
                     col_types = cols("gw_start" = col_date("%d:%m:%Y"), "gw_end" = col_date("%d:%m:%Y")))
 
 # remove duplicates
@@ -16,26 +18,28 @@ gw_set <- gwcodes %>%
   group_by(gwn, gwc) %>%
   summarize() 
 
-new_data <- countrycode_data %>%
+countrycode_data <- countrycode_data %>%
   left_join(gw_set, by = c("cown" = "gwn"))
 
-new_data %>%
+countrycode_data %>%
   filter(cowc != gwc) 
 
-new_data <- new_data %>%
-  mutate(gwn = cown,
-         iso2c = ifelse(country.name=='Namibia', 'NA', iso2c))
+# find missing
+get_missing_data <- function(countrycodes, gwcodes) {
+  gwcodes %>%
+    anti_join(countrycodes, by = c("gwn" = "cown")) %>%
+    group_by(gwn, gwc, gw_country) %>%
+    summarize()
+}
 
-       
+missing_data <- get_missing_data(countrycode_data, gwcodes)
 
-
-
-gwcodes %>%
-  anti_join(new_data, by = c("gwn" = "cown")) %>%
-  filter(!is.na(gwn)) %>%
-  arrange(gwn) %>%
-  select(gwn, gwc, gw_country)
-
+countrycode_data <- countrycode_data %>%
+  left_join(missing_data, by = c("country.name" = "gw_country")) %>%
+  mutate(gwn = ifelse(is.na(gwn), cown, gwn),
+         gwc = ifelse(is.na(gwc.x), gwc.y, gwc.x)) %>%
+  select(-c(gwc.x, gwc.y))
+  
 save(countrycode_data, file='data/countrycode_data.rda')
 
 
